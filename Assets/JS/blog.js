555b83fd931698a4c25ec69863220d7af27e54e2
Aneri Patel <aneripatel2002@gmail.com>
2024-04-16 18:42:25 -0400

    more changes

--- Assets/JS/blog.js
+++ Assets/JS/blog.js
@@ -1,29 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     const blogList = document.getElementById('blogList');
-    
+
     // Toggle between Dark Mode and Light Mode
     const toggleBtn = document.getElementById('toggleBtn');
-  
+
     if (toggleBtn) {
         toggleBtn.addEventListener('click', function() {
             const body = document.body;
-            body.classList.toggle('light-mode'); 
-            body.classList.toggle('dark-mode'); 
+            body.classList.toggle('light-mode');
+            body.classList.toggle('dark-mode');
         });
     }
 
-    // show blogs 
+    // Show blogs
     const blogs = JSON.parse(localStorage.getItem('blogs')) || [];
-  
+
     // Function to display blogs
     function displayBlogs() {
-        blogList.innerHTML = ''; 
-  
+        blogList.innerHTML = '';
+
         blogs.forEach(function(blog) {
-            
             const postContainer = document.createElement('div');
             postContainer.classList.add('blog-post-container');
-  
+
             const blogPost = document.createElement('div');
             blogPost.classList.add('blog-post');
             blogPost.innerHTML = `
@@ -31,12 +30,11 @@ document.addEventListener('DOMContentLoaded', function() {
                 <p class="author">By: ${blog.username}</p>
                 <p class="content">${blog.content}</p>
             `;
-  
+
             postContainer.appendChild(blogPost);
             blogList.appendChild(postContainer);
         });
     }
-      
+
     displayBlogs();
-    
-  });
+});
--- Assets/JS/logic.js
+++ Assets/JS/logic.js
@@ -1,51 +1,52 @@
 document.addEventListener('DOMContentLoaded', function() {
     let darkMode = true;
+
     const submitBtn = document.getElementById('submitBtn');
-    if(submitBtn) {
+    if (submitBtn) {
         submitBtn.addEventListener('click', function(event) {
             event.preventDefault();
-        
+
             const username = document.getElementById('username').value;
             const blogTitle = document.getElementById('blogTitle').value;
             const blogContent = document.getElementById('blogContent').value;
-        
+
             if (!username || !blogTitle || !blogContent) {
-                alert('Please complete all fields.'); 
-                return; 
+                alert('Please complete all fields.');
+                return;
             }
-                    
+
             const data = {
                 username: username,
                 title: blogTitle,
                 content: blogContent
             };
-        
-            // Retrieve existing blogs from local Storage 
+
+            // Retrieve existing blogs from localStorage
             const blogs = JSON.parse(localStorage.getItem('blogs')) || [];
-            
+
             // Add the new blog to the array
             blogs.unshift(data);
-            
-            // Store the updated array back in local Storage
+
+            // Store the updated array back in localStorage
             localStorage.setItem('blogs', JSON.stringify(blogs));
-        
+
             // Redirect to blog.html
             window.location.href = 'blog.html';
         });
     }
-    
-    const toggleBtn = document.getElementById('toggleBtn');
 
+    const toggleBtn = document.getElementById('toggleBtn');
     if (toggleBtn) {
         toggleBtn.addEventListener('click', function() {
             const body = document.body;
-            if (darkMode){
-                body.setAttribute('class', 'light-mode');
-                darkMode=false;
-            }
-            else
-            {body.setAttribute('class', 'dark-mode');
-                darkMode=true;
+            if (darkMode) {
+                body.classList.remove('dark-mode');
+                body.classList.add('light-mode');
+                darkMode = false;
+            } else {
+                body.classList.remove('light-mode');
+                body.classList.add('dark-mode');
+                darkMode = true;
             }
         });
     }
